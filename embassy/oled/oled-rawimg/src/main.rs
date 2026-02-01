444fb5e07629dcd6cf84761011c64b1d5dbb5c36
ImplFerris <implferris@proton.me>
2026-02-01 21:05:38 +0530

    oled raw image

---
+++ embassy/oled/oled-rawimg/src/main.rs
@@ -0,0 +1,92 @@
+#![no_std]
+#![no_main]
+
+use embassy_executor::Spawner;
+use embassy_time::Timer;
+
+// defmt Logging
+use defmt::info;
+use defmt_rtt as _;
+
+use panic_probe as _;
+
+// Interrupt Binding
+use embassy_rp::peripherals::I2C0;
+use embassy_rp::{bind_interrupts, i2c};
+
+// I2C
+use embassy_rp::i2c::{Config as I2cConfig, I2c};
+
+// OLED
+use ssd1306::{I2CDisplayInterface, Ssd1306Async, prelude::*};
+
+// Embedded Graphics
+use embedded_graphics::{
+    image::{Image, ImageRaw},
+    pixelcolor::BinaryColor,
+    prelude::Point,
+    prelude::*,
+};
+
+bind_interrupts!(struct Irqs {
+    I2C0_IRQ => i2c::InterruptHandler<I2C0>;
+});
+
+// 31x7 pixel
+#[rustfmt::skip]
+const IMG_DATA: &[u8] = &[
+    // 1st row
+    0b00000001,0b11111111,0b11111111,0b00000000,
+    // 2nd row
+    0b00000001,0b11111111,0b11111111,0b00000000,
+    //3rd row
+    0b00000001,0b10000000,0b00000011,0b00000000,
+    //4th row
+    0b11111111,0b10000000,0b00000011,0b11111110,
+    //5th row
+    0b00000001,0b10000000,0b00000011,0b00000000,
+    //6th row
+    0b00000001,0b11111111,0b11111111,0b00000000,
+    //7th row
+    0b00000001,0b11111111,0b11111111,0b00000000,
+];
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    info!("Initializing the program");
+
+    let sda = p.PIN_16;
+    let scl = p.PIN_17;
+
+    let mut i2c_config = I2cConfig::default();
+    i2c_config.frequency = 400_000; // 400kHz
+
+    let i2c_bus = I2c::new_async(p.I2C0, scl, sda, Irqs, i2c_config);
+
+    let i2c_interface = I2CDisplayInterface::new(i2c_bus);
+
+    let mut display = Ssd1306Async::new(i2c_interface, DisplaySize128x64, DisplayRotation::Rotate0)
+        .into_buffered_graphics_mode();
+
+    display
+        .init()
+        .await
+        .expect("failed to initialize the display");
+
+    let raw_image = ImageRaw::<BinaryColor>::new(IMG_DATA, 31);
+    let image = Image::new(&raw_image, Point::new(35, 35));
+
+    image
+        .draw(&mut display)
+        .expect("failed to draw text to display");
+    display
+        .flush()
+        .await
+        .expect("failed to flush data to display");
+
+    loop {
+        Timer::after_secs(1).await;
+    }
+}
